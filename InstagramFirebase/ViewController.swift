d2f7891ac1714a7eef6c44748729cecfd1841fe2
Anant S Awasthy <anant@binarynumbers.io>
2018-07-26 06:51:24 +0530

    Initial Commit

---
+++ InstagramFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramFirebase
+//
+//  Created by anantawasthy on 26/07/18.
+//  Copyright © 2018 anantawasthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
